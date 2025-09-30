f1378fe52e245ae95a172c93e52c0618c43766d0
Gabriel Henrique Lins <130785312+gabrielhilins@users.noreply.github.com>
2025-09-30 19:30:11 -0300

    adicionando 2 novos certificados

--- src/lib/certificates-data.ts
+++ src/lib/certificates-data.ts
@@ -1,13 +1,15 @@
 // src/lib/certificates-data.ts
 import { Certificate } from './types';
-import Wadhwani from '../../public/Certificado WadhWaniani.png'
-import JavaDIO from '../../public/Certificado Bootcamp Santander.png'
-import Git from '../../public/Certificado Git.png'
-import MongoDB from '../../public/Certificado Mongodb.png'
-import Python from '../../public/Certificado Python.png'
-import Globo from '../../public/Certificado Globo.png'
-import MagaluCloud from '../../public/Certificado RFEC_MagaluCloud.png'
-import InglesEasy from '../../public/Ingles Easy.png'
+import Wadhwani from '../../public/Certificado WadhWaniani.png';
+import JavaDIO from '../../public/Certificado Bootcamp Santander.png';
+import Git from '../../public/Certificado Git.png';
+import MongoDB from '../../public/Certificado Mongodb.png';
+import Python from '../../public/Certificado Python.png';
+import Globo from '../../public/Certificado Globo.png';
+import MagaluCloud from '../../public/Certificado RFEC_MagaluCloud.png';
+import InglesEasy from '../../public/Ingles Easy.png';
+import UPEDestaca from '../../public/Certificado UPE Destaca 2025.png';
+import UFPENoMercado from '../../public/Certificado UFPE no mercado 2024.png';
 
 const rawCertificatesData: Omit<Certificate, 'title' | 'description' | 'issuer'>[] = [
   {
@@ -17,7 +19,7 @@ const rawCertificatesData: Omit<Certificate, 'title' | 'description' | 'issuer'>
     image: MagaluCloud,
     pdfUrl: "https://drive.google.com/file/d/12EQw0PxE6DoLZyjZFweYXJfbqDtB8oKW/view?usp=sharing",
     issuerKey: "cert_magalu_issuer", 
-    issueDate: "2025-04-21",
+    issueDate: "2025-08-20",
   },
   {
     id: 2, 
@@ -81,6 +83,24 @@ const rawCertificatesData: Omit<Certificate, 'title' | 'description' | 'issuer'>
     pdfUrl: "https://drive.google.com/file/d/1Dw4UsGl-nAQfdfagu_3fyh9P8pe8ttYx/view?usp=sharing",
     issuerKey: "cert_globo_issuer", 
     issueDate: "2023-10-21", 
+  },
+  {
+    id: 8, 
+    titleKey: "cert_ufpe_no_mercado_2024_title",
+    descriptionKey: "cert_ufpe_no_mercado_2024_desc",
+    image: UFPENoMercado,
+    pdfUrl: "https://drive.google.com/file/d/1gUoPUG5G9P6rC9iKd-ZPJg97rk1HDwaD/view?usp=sharing",
+    issuerKey: "cert_ufpe_no_mercado_2024_issuer", 
+    issueDate: "2024-11-13", 
+  },
+  {
+    id: 9, 
+    titleKey: "cert_upe_destaca_title",
+    descriptionKey: "cert_upe_destaca_desc",
+    image: UPEDestaca,
+    pdfUrl: "https://drive.google.com/file/d/13T3TgKHDb6-ZEdt-bB_DJ5rmieLC1MKA/view?usp=sharing",
+    issuerKey: "cert_upe_destaca_issuer", 
+    issueDate: "2025-06-11", 
   },
 ];
 
